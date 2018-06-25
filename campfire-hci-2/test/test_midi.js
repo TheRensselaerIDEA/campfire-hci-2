092ab25a42cc32829e9dd9a9563f180db80cee18
Antonio Fiol-Mahon <afiolmahon@gmail.com>
2018-06-25 11:56:35 -0400

    Add Midi start/stop test

---
+++ campfire-hci-2/test/test_midi.js
@@ -0,0 +1,19 @@
+'use strict';
+const MidiHandler = require('../MidiHandler.js');
+
+console.log('Midi Test...');
+
+console.log('Creating Midi object...');
+
+// Create and start controller
+var mh = new MidiHandler();
+
+// Bind button
+mh.bindButtonReleaseHandler(mh.BTN_CODE.BTN_01, (val) => {
+    console.log("button pressed!");
+});
+
+// Stopping controller 
+mh.stop();
+// Starting again
+mh.start();
